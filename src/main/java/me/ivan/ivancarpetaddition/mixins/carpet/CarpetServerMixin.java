efa4d637c9f66aef1cde34d4c937558584ecf5e5
Ivan-YFw <wyf0762@foxmail.com>
2021-01-26 20:38:05 +0800

    fix /carpet

---
+++ src/main/java/me/ivan/ivancarpetaddition/mixins/carpet/CarpetServerMixin.java
@@ -0,0 +1,23 @@
+package me.ivan.ivancarpetaddition.mixins.carpet;
+
+import carpet.CarpetServer;
+import me.ivan.ivancarpetaddition.IvanCarpetAdditionServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+
+@Mixin(CarpetServer.class)
+public abstract class CarpetServerMixin
+{
+	@Inject(
+			method = "onGameStarted",
+			at = @At(value = "HEAD"),
+			remap = false
+	)
+	private static void registerCarpetExtension(CallbackInfo ci)
+	{
+		IvanCarpetAdditionServer.noop();
+	}
+}
