fc8d02a275e67fe331e140c52b4768914c64aa8c
Ivan-YFw <wyf0762@foxmail.com>
2021-01-26 20:26:54 +0800

    change customVersion category

--- src/main/java/me/ivan/ivancarpetaddition/IvanCarpetAdditionSettings.java
+++ src/main/java/me/ivan/ivancarpetaddition/IvanCarpetAdditionSettings.java
@@ -2,7 +2,7 @@ package me.ivan.ivancarpetaddition;
 
 import carpet.settings.Rule;
 
-import static carpet.settings.RuleCategory.CREATIVE;
+import static carpet.settings.RuleCategory.*;
 
 public class IvanCarpetAdditionSettings {
     public static final String ICA = "ICA";
@@ -12,7 +12,7 @@ public class IvanCarpetAdditionSettings {
             extra = "use '_' to disable",
             options = "_",
             strict = false,
-            category = {ICA, CREATIVE}
+            category = {ICA, EXPERIMENTAL}
     )
     public static String customVersion = "_";
 }
