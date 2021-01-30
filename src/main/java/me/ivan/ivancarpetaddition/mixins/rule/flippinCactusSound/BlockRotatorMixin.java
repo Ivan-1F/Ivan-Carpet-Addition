74fefeb350c74ce6e761fecf79fcf013a07f376c
Ivan-YFw <wyf0762@foxmail.com>
2021-01-30 11:59:34 +0800

    flippinCactusSound fixed

--- src/main/java/me/ivan/ivancarpetaddition/mixins/rule/flippinCactusSound/BlockRotatorMixin.java
+++ src/main/java/me/ivan/ivancarpetaddition/mixins/rule/flippinCactusSound/BlockRotatorMixin.java
@@ -1,7 +1,6 @@
 package me.ivan.ivancarpetaddition.mixins.rule.flippinCactusSound;
 
 import carpet.helpers.BlockRotator;
-import me.ivan.ivancarpetaddition.IvanCarpetAdditionServer;
 import me.ivan.ivancarpetaddition.IvanCarpetAdditionSettings;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
@@ -13,19 +12,18 @@ import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BlockRotator.class)
 public class BlockRotatorMixin {
     @Inject(
-            method = "flipBlockWithCactus",
+            method = "flip_block",
             at = @At(
                     value = "INVOKE",
-                    target = "Lcarpet/helpers/BlockRotator;flip_block(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/World;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/Hand;Lnet/minecraft/util/hit/BlockHitResult;)Z"),
-            remap = false
+                    target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z"
+            )
     )
-    private static void flipBlockWithCactus(BlockState state, World world, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<Boolean> cir) {
+    private static void flip_block(BlockState state, World world, PlayerEntity player, Hand hand, BlockHitResult hit, CallbackInfoReturnable<Boolean> cir) {
         if (IvanCarpetAdditionSettings.flippinCactusSound) {
             player.playSound(SoundEvents.BLOCK_DISPENSER_LAUNCH, SoundCategory.AMBIENT, 1.0F, 1.0F);
         }
