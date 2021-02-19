8f9af8fc72860bf5199ac4a6b4ab9fda962f5229
Ivan-YFw <wyf0762@foxmail.com>
2021-02-19 14:49:00 +0800

    1.16

--- src/main/java/me/ivan/ivancarpetaddition/mixins/rule/blockEventChunkLoading/ServerWorldMixin.java
+++ src/main/java/me/ivan/ivancarpetaddition/mixins/rule/blockEventChunkLoading/ServerWorldMixin.java
@@ -2,7 +2,7 @@ package me.ivan.ivancarpetaddition.mixins.rule.blockEventChunkLoading;
 
 import me.ivan.ivancarpetaddition.IvanCarpetAdditionSettings;
 import me.ivan.ivancarpetaddition.utils.registry.ChunkTicketTypeRegistry;
-import net.minecraft.server.world.BlockAction;
+import net.minecraft.server.world.BlockEvent;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
@@ -14,8 +14,8 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(ServerWorld.class)
 public class ServerWorldMixin {
-    @Inject(method = "method_14174", at = @At("RETURN"), locals = LocalCapture.CAPTURE_FAILHARD)
-    private void afterBlockEventExecuted(BlockAction blockAction, CallbackInfoReturnable<Boolean> cir) {
+    @Inject(method = "processBlockEvent", at = @At("RETURN"), locals = LocalCapture.CAPTURE_FAILHARD)
+    private void afterBlockEventExecuted(BlockEvent blockAction, CallbackInfoReturnable<Boolean> cir) {
         if (!IvanCarpetAdditionSettings.blockEventChunkLoading) return;
         BlockPos pos = blockAction.getPos();
         ServerWorld world = (ServerWorld)(Object) this;
