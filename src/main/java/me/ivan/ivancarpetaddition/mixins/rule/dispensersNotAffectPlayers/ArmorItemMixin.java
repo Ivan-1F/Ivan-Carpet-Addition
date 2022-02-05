3ea58e471b4b57c729d674aec90db438e9c5c4e0
Ivan1F <wyf0762@foxmail.com>
2022-02-05 23:17:54 +0800

    1.16

--- src/main/java/me/ivan/ivancarpetaddition/mixins/rule/dispensersNotAffectPlayers/ArmorItemMixin.java
+++ src/main/java/me/ivan/ivancarpetaddition/mixins/rule/dispensersNotAffectPlayers/ArmorItemMixin.java
@@ -5,7 +5,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.util.math.Box;
-import net.minecraft.world.World;
+import net.minecraft.server.world.ServerWorld;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,10 +16,10 @@ import java.util.function.Predicate;
 
 @Mixin(ArmorItem.class)
 public class ArmorItemMixin {
-    @Redirect(method = "dispenseArmor", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getEntities(Ljava/lang/Class;Lnet/minecraft/util/math/Box;Ljava/util/function/Predicate;)Ljava/util/List;"))
-    private static <T extends Entity> List<T> removePlayer(World instance, Class<? extends T> entityClass, Box box, @Nullable Predicate<? super T> predicate) {
+    @Redirect(method = "dispenseArmor", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;getEntitiesByClass(Ljava/lang/Class;Lnet/minecraft/util/math/Box;Ljava/util/function/Predicate;)Ljava/util/List;"))
+    private static <T extends Entity> List<T> removePlayer(ServerWorld instance, Class<? extends T> entityClass, Box box, @Nullable Predicate<? super T> predicate) {
         // Vanilla
-        List<T> entities = instance.getEntities(entityClass, box, predicate);
+        List<T> entities = instance.getEntitiesByClass(entityClass, box, predicate);
         if (IvanCarpetAdditionSettings.dispensersNotAffectPlayers) {
             entities.removeIf(entity -> entity instanceof PlayerEntity);
         }
