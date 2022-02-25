2f057f66b3820e79705b2683c957c06c9be2ae09
Ivan1F <wyf0762@foxmail.com>
2022-02-25 20:49:02 +0800

    14

--- src/main/java/me/ivan/ivancarpetaddition/mixins/rule/stopFreezing/BiomeMixin.java
+++ src/main/java/me/ivan/ivancarpetaddition/mixins/rule/stopFreezing/BiomeMixin.java
@@ -2,7 +2,7 @@ package me.ivan.ivancarpetaddition.mixins.rule.stopFreezing;
 
 import me.ivan.ivancarpetaddition.IvanCarpetAdditionSettings;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.WorldView;
+import net.minecraft.world.CollisionView;
 import net.minecraft.world.biome.Biome;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,8 +11,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Biome.class)
 public class BiomeMixin {
-    @Inject(method = "canSetIce(Lnet/minecraft/world/WorldView;Lnet/minecraft/util/math/BlockPos;Z)Z", at = @At("HEAD"), cancellable = true)
-    private void stopFreezing(WorldView world, BlockPos pos, boolean doWaterCheck, CallbackInfoReturnable<Boolean> cir) {
+    @Inject(method = "canSetIce", at = @At("HEAD"), cancellable = true)
+    private void stopFreezing(CollisionView world, BlockPos blockPos, CallbackInfoReturnable<Boolean> cir) {
         if (IvanCarpetAdditionSettings.stopFreezing) cir.setReturnValue(false);
     }
 }
