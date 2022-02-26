1101ebca114099cab96a4936ca47cee716d9bf94
Ivan1F <wyf0762@foxmail.com>
2022-02-26 09:28:20 +0800

    more reasons for xp counter

--- src/main/java/me/ivan/ivancarpetaddition/commands/xpcounter/SpawnReason.java
+++ src/main/java/me/ivan/ivancarpetaddition/commands/xpcounter/SpawnReason.java
@@ -6,6 +6,12 @@ import me.ivan.ivancarpetaddition.translations.Translator;
 public class SpawnReason extends TranslatableBase {
     public static final SpawnReason EXPERIENCE_BOTTLE = new SpawnReason("experience_bottle");
     public static final SpawnReason ENTITY_LOOT = new SpawnReason("entity_loot");
+    public static final SpawnReason FURNACE = new SpawnReason("furnace");
+    public static final SpawnReason BLOCK = new SpawnReason("block");
+    public static final SpawnReason ENDER_DRAGON = new SpawnReason("ender_dragon");
+    public static final SpawnReason BREEDING = new SpawnReason("breeding");
+    public static final SpawnReason FISHING = new SpawnReason("fishing");
+    public static final SpawnReason TRADING = new SpawnReason("trading");
 
     private final String translationKey;
 
