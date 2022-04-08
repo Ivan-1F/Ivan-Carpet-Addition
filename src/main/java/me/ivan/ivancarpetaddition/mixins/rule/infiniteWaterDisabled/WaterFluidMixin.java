d4fc2a96f985767491e6a228825968b1cca185b9
Ivan1F <wyf0762@foxmail.com>
2022-04-08 09:10:44 +0800

    fixed injection error of infiniteWaterDisabled

--- src/main/java/me/ivan/ivancarpetaddition/mixins/rule/infiniteWaterDisabled/WaterFluidMixin.java
+++ src/main/java/me/ivan/ivancarpetaddition/mixins/rule/infiniteWaterDisabled/WaterFluidMixin.java
@@ -4,11 +4,14 @@ import me.ivan.ivancarpetaddition.IvanCarpetAdditionSettings;
 import net.minecraft.fluid.BaseFluid;
 import net.minecraft.fluid.WaterFluid;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(WaterFluid.class)
 public abstract class WaterFluidMixin extends BaseFluid {
-    @Override
-    protected boolean isInfinite() {
-        return !IvanCarpetAdditionSettings.infiniteWaterDisabled;
+    @Inject(method = "isInfinite", at = @At("HEAD"), cancellable = true)
+    private void isInfinite(CallbackInfoReturnable<Boolean> cir) {
+        if (IvanCarpetAdditionSettings.infiniteWaterDisabled) cir.setReturnValue(false);
     }
 }
