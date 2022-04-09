6925611abbd2f692fc7bb75065a80b43deb18e99
Ivan1F <wyf0762@foxmail.com>
2022-04-09 16:08:19 +0800

    22w14a

--- src/main/java/me/ivan/ivancarpetaddition/mixins/rule/mobAlwaysPickUpLoot/ZombieEntityMixin.java
+++ src/main/java/me/ivan/ivancarpetaddition/mixins/rule/mobAlwaysPickUpLoot/ZombieEntityMixin.java
@@ -2,16 +2,22 @@ package me.ivan.ivancarpetaddition.mixins.rule.mobAlwaysPickUpLoot;
 
 import me.ivan.ivancarpetaddition.IvanCarpetAdditionSettings;
 import net.minecraft.entity.mob.ZombieEntity;
+import net.minecraft.util.math.random.AbstractRandom;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import java.util.Random;
-
 @Mixin(ZombieEntity.class)
 public class ZombieEntityMixin {
-    @Redirect(method = "initialize", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextFloat()F", ordinal = 0))
-    private float alwaysReturnsNegative(Random instance) {
+    @Redirect(
+            method = "initialize",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/util/math/random/AbstractRandom;nextFloat()F",
+                    ordinal = 0
+            )
+    )
+    private float alwaysReturnsNegative(AbstractRandom instance) {
         if (IvanCarpetAdditionSettings.mobAlwaysPickUpLoot) {
             return -1;
         }
--- src/main/java/me/ivan/ivancarpetaddition/mixins/rule/unbreakableHelmetInSunlight/AbstractSkeletonEntityMixin.java
+++ src/main/java/me/ivan/ivancarpetaddition/mixins/rule/unbreakableHelmetInSunlight/AbstractSkeletonEntityMixin.java
@@ -2,16 +2,21 @@ package me.ivan.ivancarpetaddition.mixins.rule.unbreakableHelmetInSunlight;
 
 import me.ivan.ivancarpetaddition.IvanCarpetAdditionSettings;
 import net.minecraft.entity.mob.AbstractSkeletonEntity;
+import net.minecraft.util.math.random.AbstractRandom;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import java.util.Random;
-
 @Mixin(AbstractSkeletonEntity.class)
 public class AbstractSkeletonEntityMixin {
-    @Redirect(method = "tickMovement", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextInt(I)I"))
-    private int stopDamagingHelmet(Random instance, int bound) {
+    @Redirect(
+            method = "tickMovement",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/util/math/random/AbstractRandom;nextInt(I)I"
+            )
+    )
+    private int stopDamagingHelmet(AbstractRandom instance, int bound) {
         return IvanCarpetAdditionSettings.unbreakableHelmetInSunlight ? 0 : instance.nextInt(bound);
     }
 }
