552c5687a561ae453dee3d0c477a67130138a6a1
Ivan1F <wyf0762@foxmail.com>
2022-04-10 16:55:05 +0800

    1.16

--- src/main/java/me/ivan/ivancarpetaddition/mixins/carpet/HUDControllerMixin.java
+++ src/main/java/me/ivan/ivancarpetaddition/mixins/carpet/HUDControllerMixin.java
@@ -4,21 +4,17 @@ import carpet.logging.HUDController;
 import me.ivan.ivancarpetaddition.logging.ICAHUDController;
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.List;
+import java.util.function.Consumer;
 
 @Mixin(HUDController.class)
 public class HUDControllerMixin {
-    @Inject(
-            method = "update_hud",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Ljava/util/Map;keySet()Ljava/util/Set;"
-            ),
-            remap = false
-    )
-    private static void updateTISAdditionHUDLoggers(MinecraftServer server, CallbackInfo ci) {
-        ICAHUDController.updateHUD(server);
+    @Shadow(remap = false)
+    private static List<Consumer<MinecraftServer>> HUDListeners;
+
+    static {
+        HUDListeners.add(ICAHUDController::updateHUD);
     }
 }
--- src/main/java/me/ivan/ivancarpetaddition/mixins/translations/HUDControllerMixin.java
+++ src/main/java/me/ivan/ivancarpetaddition/mixins/translations/HUDControllerMixin.java
@@ -2,7 +2,6 @@ package me.ivan.ivancarpetaddition.mixins.translations;
 
 import carpet.logging.HUDController;
 import me.ivan.ivancarpetaddition.translations.ICATranslations;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.BaseText;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,7 +11,10 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 @Mixin(HUDController.class)
 public abstract class HUDControllerMixin {
     @ModifyVariable(method = "addMessage", at = @At("HEAD"), argsOnly = true, remap = false)
-    private static BaseText applyTISCarpetTranslationToHudLoggerMessage(BaseText hudMessage, /* parent method parameters -> */ PlayerEntity player, BaseText hudMessage_) {
-        return ICATranslations.translate(hudMessage, (ServerPlayerEntity) player);
+    private static BaseText applyTISCarpetTranslationToHudLoggerMessage(BaseText hudMessage, /* parent method parameters -> */ ServerPlayerEntity player, BaseText hudMessage_) {
+        if (player != null) {
+            hudMessage = ICATranslations.translate(hudMessage, player);
+        }
+        return hudMessage;
     }
 }
