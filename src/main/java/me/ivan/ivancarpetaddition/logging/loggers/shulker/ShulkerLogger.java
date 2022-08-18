ee7fce3e3ea43a6f9d487f080550677f614144cf
Ivan1F <wyf0762@foxmail.com>
2022-08-18 20:09:36 +0800

    - shulker logger - Messenger stuff (credit: Carpet TIS Addition)

---
+++ src/main/java/me/ivan/ivancarpetaddition/logging/loggers/shulker/ShulkerLogger.java
@@ -0,0 +1,78 @@
+package me.ivan.ivancarpetaddition.logging.loggers.shulker;
+
+import com.google.common.collect.Lists;
+import me.ivan.ivancarpetaddition.logging.loggers.AbstractLogger;
+import me.ivan.ivancarpetaddition.utils.Messenger;
+import net.minecraft.entity.mob.ShulkerEntity;
+import net.minecraft.text.BaseText;
+import net.minecraft.util.math.BlockPos;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ShulkerLogger extends AbstractLogger {
+    private static final String NAME = "shulker";
+    private static final ShulkerLogger INSTANCE = new ShulkerLogger();
+
+    private ShulkerLogger() {
+        super(NAME, true);
+    }
+
+    public static ShulkerLogger getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public @Nullable String getDefaultLoggingOption() {
+        return LoggingOption.DEFAULT.getName();
+    }
+
+    @Override
+    public @Nullable String[] getSuggestedLoggingOption() {
+        return LoggingOption.getSuggestions();
+    }
+
+    public void onShulkerTeleport(ShulkerEntity shulker, BlockPos from, BlockPos to) {
+        this.log(option -> {
+            if (LoggingOption.TELEPORT.isContainedIn(option)) {
+                return new BaseText[]{
+                        Messenger.c(
+                                "g [",
+                                Messenger.s(shulker.getEntityWorld().getTime(), "g"),
+                                "g ] ",
+                                Messenger.entity("g", shulker),
+                                "g  @ ",
+                                Messenger.coord(from),
+                                "g  -> ",
+                                Messenger.coord(to)
+                        )
+                };
+            }
+            return null;
+        });
+    }
+
+    public enum LoggingOption {
+        DUPE,
+        TELEPORT;
+
+        public static final LoggingOption DEFAULT = TELEPORT;
+
+        public String getName() {
+            return this.name().toLowerCase();
+        }
+
+        public static String[] getSuggestions() {
+            List<String> suggestions = Lists.newArrayList();
+            suggestions.addAll(Arrays.stream(values()).map(LoggingOption::getName).collect(Collectors.toList()));
+            suggestions.add(createCompoundOption(DUPE.getName(), TELEPORT.getName()));
+            return suggestions.toArray(new String[0]);
+        }
+
+        public boolean isContainedIn(String option) {
+            return Arrays.asList(option.split(MULTI_OPTION_SEP_REG)).contains(this.getName());
+        }
+    }
+}
