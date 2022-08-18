69de6377bb6545727cc6e95ead5a8169eb45355e
Ivan1F <wyf0762@foxmail.com>
2022-08-19 02:57:01 +0800

    1.19.1

--- src/main/java/me/ivan/ivancarpetaddition/mixins/carpet/SettingsManagerMixin.java
+++ src/main/java/me/ivan/ivancarpetaddition/mixins/carpet/SettingsManagerMixin.java
@@ -1,6 +1,6 @@
 package me.ivan.ivancarpetaddition.mixins.carpet;
 
-import carpet.settings.SettingsManager;
+import carpet.api.settings.SettingsManager;
 import carpet.utils.Messenger;
 import me.ivan.ivancarpetaddition.IvanCarpetAdditionMod;
 import me.ivan.ivancarpetaddition.IvanCarpetAdditionServer;
@@ -8,26 +8,17 @@ import net.minecraft.server.command.ServerCommandSource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import static carpet.utils.Translations.tr;
 
 @Mixin(SettingsManager.class)
 public class SettingsManagerMixin {
-    @SuppressWarnings("DefaultAnnotationParam")
     @Inject(
             method = "listAllSettings",
-            slice = @Slice(
-                    from = @At(
-                            value = "CONSTANT",
-                            args = "stringValue=ui.version",  // after printed fabric-carpet version
-                            ordinal = 0
-                    )
-            ),
             at = @At(
                     value = "INVOKE",
-                    target = "Lcarpet/settings/SettingsManager;getCategories()Ljava/lang/Iterable;",
+                    target = "Lcarpet/api/settings/SettingsManager;getCategories()Ljava/lang/Iterable;",
                     ordinal = 0
             ),
             remap = false
