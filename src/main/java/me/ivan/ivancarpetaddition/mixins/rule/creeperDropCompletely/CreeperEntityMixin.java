2369f286410792d4101b7c3c568a00a7344f955f
Ivan1F <wyf0762@foxmail.com>
2022-12-24 14:58:19 +0800

    updated CreeperEntityMixin

--- src/main/java/me/ivan/ivancarpetaddition/mixins/rule/creeperDropCompletely/CreeperEntityMixin.java
+++ src/main/java/me/ivan/ivancarpetaddition/mixins/rule/creeperDropCompletely/CreeperEntityMixin.java
@@ -1,14 +1,11 @@
 package me.ivan.ivancarpetaddition.mixins.rule.creeperDropCompletely;
 
 import me.ivan.ivancarpetaddition.IvanCarpetAdditionSettings;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.mob.CreeperEntity;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
-import net.minecraft.world.explosion.Explosion;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -20,7 +17,7 @@ public abstract class CreeperEntityMixin extends HostileEntity {
     }
 
     @ModifyArg(method = "explode", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;createExplosion(Lnet/minecraft/entity/Entity;DDDFLnet/minecraft/world/World$ExplosionSourceType;)Lnet/minecraft/world/explosion/Explosion;"))
-    private World.ExplosionSourceType setDestructionType(@Nullable Entity entity) {
+    private World.ExplosionSourceType setDestructionType(World.ExplosionSourceType explosionSourceType) {
         return this.world.getGameRules().getBoolean(GameRules.DO_MOB_GRIEFING) ? IvanCarpetAdditionSettings.creeperDropCompletely ? World.ExplosionSourceType.MOB : World.ExplosionSourceType.TNT : World.ExplosionSourceType.NONE;
     }
 }
