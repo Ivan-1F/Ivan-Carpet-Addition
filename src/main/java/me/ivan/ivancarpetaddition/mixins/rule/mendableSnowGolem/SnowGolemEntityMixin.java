98c722350c4900d8732027671cb3170d69af3d9b
Ivan1F <wyf0762@foxmail.com>
2022-12-27 15:53:38 +0800

    fixed interactMob injection return type for 1.16+ (resolved #16)

--- src/main/java/me/ivan/ivancarpetaddition/mixins/rule/mendableSnowGolem/SnowGolemEntityMixin.java
+++ src/main/java/me/ivan/ivancarpetaddition/mixins/rule/mendableSnowGolem/SnowGolemEntityMixin.java
@@ -2,21 +2,29 @@ package me.ivan.ivancarpetaddition.mixins.rule.mendableSnowGolem;
 
 import me.ivan.ivancarpetaddition.IvanCarpetAdditionSettings;
 import me.ivan.ivancarpetaddition.helpers.rule.mendableGolem.MendableGolemHelper;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.SnowGolemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(SnowGolemEntity.class)
-public class SnowGolemEntityMixin {
+public abstract class SnowGolemEntityMixin extends Entity {
+    public SnowGolemEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
     @Inject(method = "interactMob", at = @At("HEAD"), cancellable = true)
-    private void mendSnowGolem(PlayerEntity player, Hand hand, CallbackInfoReturnable<Boolean> cir) {
+    private void mendSnowGolem(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
         ItemStack itemStack = player.getStackInHand(hand);
         if (itemStack.getItem() == Items.SNOWBALL && IvanCarpetAdditionSettings.mendableSnowGolem) {
             cir.setReturnValue(MendableGolemHelper.mendGolemFromStack(
@@ -25,7 +33,7 @@ public class SnowGolemEntityMixin {
                     SoundEvents.BLOCK_SNOW_PLACE,
                     player,
                     itemStack
-            ));
+            ) ? ActionResult.success(this.world.isClient) : ActionResult.PASS);
         }
     }
 }
